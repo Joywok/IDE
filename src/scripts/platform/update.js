b7c18741ad439a57b89477de3cc3978d1d44a9ee
zhailei <zhailei2011@gmail.com>
2017-01-16 09:50:07 +0800

    fix backup

--- src/scripts/platform/update.js
+++ src/scripts/platform/update.js
@@ -36,7 +36,9 @@ window.newNotication = function(){
 				  console.log('Caught an error');
 				});
 				unzipper.on('extract', function (log) {
-				  console.log('Finished extracting');
+				  fsExtra.remove('update.tgz', function (err){});
+					newWin.close();
+					window.AppRestart();
 				});
 				unzipper.on('progress', function (fileIndex, fileCount) {
 				  console.log('Extracted file ' + (fileIndex + 1) + ' of ' + fileCount);
